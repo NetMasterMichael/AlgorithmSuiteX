6fc57198006ba1098c0842e9435dd11fad2d805a
Michael Goodwin <michael.cgdwn@gmail.com>
2024-08-03 01:55:56 +0100

    Update BubbleSort.java to implement SortingAlgorithm interface with method stubs

--- src/main/java/com/github/netmastermichael/classic_algorithms_suite/SortingAlgorithms/BubbleSort.java
+++ src/main/java/com/github/netmastermichael/classic_algorithms_suite/SortingAlgorithms/BubbleSort.java
@@ -1,8 +1,54 @@
 package com.github.netmastermichael.classic_algorithms_suite.SortingAlgorithms;
 
-public class BubbleSort {
+public class BubbleSort implements SortingAlgorithm {
+	
+	private int[] inputArray;
+	private int comparisons;
+	private int moves;
 	
 	public BubbleSort() {
+		
+	}
+
+	@Override
+	public int[] getInputArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setInputArray(int[] newInputArray) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sort() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sortWithMetrics() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void toggleInteractiveMode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isInteractiveModeEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
 		
 	}
 	
