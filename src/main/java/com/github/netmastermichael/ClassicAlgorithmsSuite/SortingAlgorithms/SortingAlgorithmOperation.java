9d607c728a787b1c5a3e290e947218e99aed75f0
Michael Goodwin <michael.cgdwn@gmail.com>
2024-08-09 20:09:26 +0100

    Add missing @author tag to Javadoc in SortingAlgorithmOperation

--- src/main/java/com/github/netmastermichael/ClassicAlgorithmsSuite/SortingAlgorithms/SortingAlgorithmOperation.java
+++ src/main/java/com/github/netmastermichael/ClassicAlgorithmsSuite/SortingAlgorithms/SortingAlgorithmOperation.java
@@ -4,6 +4,8 @@ package com.github.netmastermichael.ClassicAlgorithmsSuite.SortingAlgorithms;
  * SortingAlgorithmOperation is an enum that represents the possible types of
  * operations that a sorting algorithm may perform. Intended for use in
  * ManualSorter.
+ * 
+ * @author Michael Goodwin (NetMasterMichael)
  */
 public enum SortingAlgorithmOperation {
 	SWAP, COMPARE;
