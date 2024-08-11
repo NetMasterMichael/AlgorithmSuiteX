1f0991635ce27bd8efd12967a32b6bcc63d8d313
Michael Goodwin <michael.cgdwn@gmail.com>
2024-08-11 22:17:27 +0100

    Add Javadoc to InsertionSort

--- src/main/java/com/github/netmastermichael/ClassicAlgorithmsSuite/SortingAlgorithms/InsertionSort.java
+++ src/main/java/com/github/netmastermichael/ClassicAlgorithmsSuite/SortingAlgorithms/InsertionSort.java
@@ -3,32 +3,76 @@ package com.github.netmastermichael.ClassicAlgorithmsSuite.SortingAlgorithms;
 import java.util.Arrays;
 import java.util.LinkedList;
 
+/**
+ * InsertionSort is a class that implements the insertion sort algorithm for
+ * sorting an array of integers. It implements the SortingAlgorithm interface
+ * and is designed to primarily be used with a controller, but can be used
+ * independently.
+ * <p>
+ * Time Complexity Best Case: O(n)<br>
+ * </p>
+ * <p>
+ * Time Complexity Average Case: O(n^2)
+ * </p>
+ * <p>
+ * Time Complexity Worst Case: O(n^2)<br>
+ * </p>
+ * 
+ * @author Michael Goodwin (NetMasterMichael)
+ */
 public class InsertionSort implements SortingAlgorithm {
 
+	/** Array that is currently being worked on by the sorting algorithm */
 	private int[] inputArray;
 
+	/** SortingAlgorithmMetrics object for tracking metrics of the algorithm */
 	private SortingAlgorithmMetrics metrics;
 
+	/**
+	 * Constructor for creating a InsertionSort object to sort an array of integers.
+	 * 
+	 * @param inputArray array to sort
+	 */
 	public InsertionSort(int[] inputArray) {
 		this.inputArray = inputArray;
 		this.metrics = new SortingAlgorithmMetrics();
 	}
 
+	/**
+	 * Gets the array currently held inside the InsertionSort object.
+	 *
+	 * @return Array currently inside of InsertionSort instance
+	 */
 	@Override
 	public int[] getInputArray() {
 		return inputArray;
 	}
 
+	/**
+	 * Sets the array inside the InsertionSort object to the provided array.
+	 * 
+	 * @param Array to set inside InsertionSort instance
+	 */
 	@Override
 	public void setInputArray(int[] newInputArray) {
 		this.inputArray = newInputArray;
 	}
 
+	/**
+	 * Gets the SortingAlgorithmMetrics object containing performance metrics from
+	 * this InsertionSort object.
+	 * 
+	 * @return SortingAlgorithmMetrics object containing performance metrics
+	 */
 	@Override
 	public SortingAlgorithmMetrics getMetrics() {
 		return metrics;
 	}
 
+	/**
+	 * Sorts the array inside inputArray using the insertion sort algorithm with
+	 * optimisations. Use this method when assessing raw algorithm performance.
+	 */
 	@Override
 	public void sort() {
 		int arrayLength = inputArray.length;
@@ -46,6 +90,11 @@ public class InsertionSort implements SortingAlgorithm {
 		}
 	}
 
+	/**
+	 * Sorts the array inside inputArray using the insertion sort algorithm with
+	 * optimisations, while keeping track of metrics. Use this method when assessing
+	 * algorithm optimisation.
+	 */
 	@Override
 	public void sortWithMetrics() {
 		int arrayLength = inputArray.length;
@@ -70,12 +119,21 @@ public class InsertionSort implements SortingAlgorithm {
 		}
 	}
 
+	/**
+	 * Creates a separate instance of this algorithm as a ManualSorter object with
+	 * an independent copy of inputArray, which will be able to sort the array by
+	 * stepping through a queue of operations that correspond with the insertion
+	 * sort algorithm.
+	 * 
+	 * @return ManualSorter object queued with operations of insertion sort
+	 *         algorithm
+	 */
 	@Override
 	public ManualSorter preComputeManualSort() {
 		ManualSorter manualSorter = new ManualSorter(Arrays.copyOf(inputArray, inputArray.length),
 				new LinkedList<SortingAlgorithmOperation>(), new LinkedList<Integer>());
 		int[] inputArrayDuplicate = (Arrays.copyOf(inputArray, inputArray.length));
-		
+
 		int arrayLength = inputArrayDuplicate.length;
 		for (int i = 1; i < arrayLength; i++) {
 			int buffer = inputArrayDuplicate[i];
