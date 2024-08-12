9f08ad4508697ecaa450506af64ccaae79c7bb23
Michael Goodwin <michael.cgdwn@gmail.com>
2024-08-12 23:06:01 +0100

    Add MergeSort and Test_MergeSort classes with empty method stubs

---
+++ src/main/java/com/github/netmastermichael/ClassicAlgorithmsSuite/SortingAlgorithms/MergeSort.java
@@ -0,0 +1,41 @@
+package com.github.netmastermichael.ClassicAlgorithmsSuite.SortingAlgorithms;
+
+public class MergeSort implements SortingAlgorithm {
+
+	@Override
+	public int[] getInputArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setInputArray(int[] newInputArray) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public SortingAlgorithmMetrics getMetrics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sort() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sortWithMetrics() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ManualSorter preComputeManualSort() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
