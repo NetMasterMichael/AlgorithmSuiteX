54773cf59d278f45b0e86a45a73a3522d7320958
Michael Goodwin <michael.cgdwn@gmail.com>
2024-08-25 22:27:53 +0100

    Add Driver skeleton class

---
+++ src/main/java/com/github/netmastermichael/algorithmsuitex/Driver.java
@@ -0,0 +1,25 @@
+package com.github.netmastermichael.algorithmsuitex;
+
+/**
+ * Driver is a class that bootstraps the front-end of the application.
+ * 
+ * @author Michael Goodwin (NetMasterMichael)
+ */
+public class Driver {
+
+  /**
+   * The entry point for the AlgorithmSuiteX application.
+   * 
+   * @param args Ignored
+   */
+  public static void main(String[] args) {
+    
+    // Plan of steps for starting AlgorithmSuiteX
+    // 1. Create a controller object
+    // 2. Select an interface to load and create an object for it
+    // 3. Link the controller and interface together
+    // 4. Start/initialise the interface
+    
+  }
+
+}
