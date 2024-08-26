a24c29ec5e32015ec89a5dd9ac3f20fa34fee229
Michael Goodwin <michael.cgdwn@gmail.com>
2024-08-26 23:31:09 +0100

    Add unit tests for enqueueAlgorithm() method to TestSortingAlgorithmController

--- src/test/java/TestSortingAlgorithmController.java
+++ src/test/java/TestSortingAlgorithmController.java
@@ -1,7 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import com.github.netmastermichael.algorithmsuitex.algorithms.classic.sorting.BubbleSort;
+import com.github.netmastermichael.algorithmsuitex.algorithms.classic.sorting.InsertionSort;
+import com.github.netmastermichael.algorithmsuitex.algorithms.classic.sorting.MergeSort;
+import com.github.netmastermichael.algorithmsuitex.algorithms.classic.sorting.SelectionSort;
+import com.github.netmastermichael.algorithmsuitex.algorithms.classic.sorting.SortingAlgorithm;
+import com.github.netmastermichael.algorithmsuitex.algorithms.classic.sorting.SortingAlgorithmType;
 import com.github.netmastermichael.algorithmsuitex.controller.SortingAlgorithmController;
+import java.util.Queue;
 import org.junit.jupiter.api.Test;
 
 class TestSortingAlgorithmController {
@@ -24,4 +32,95 @@ class TestSortingAlgorithmController {
     }
   }
 
+  @Test
+  void testEnqueueBubbleSort() { // Test 02
+    String testName = "testEnqueueBubbleSort";
+    AuxiliaryTestMethods.logStartTest(className, testName);
+    try {
+      // Enqueue algorithm into a controller
+      SortingAlgorithmController testSortController = new SortingAlgorithmController();
+      testSortController.enqueueAlgorithm(SortingAlgorithmType.BUBBLE_SORT, new int[] {1, 3, 2});
+
+      // Retrieve algorithm from the controller and validate the correct algorithm is returned
+      Queue<SortingAlgorithm> tscAlgorithmQueue = testSortController.getAlgorithmQueue();
+      SortingAlgorithm algorithm = tscAlgorithmQueue.remove();
+      assertTrue(algorithm instanceof BubbleSort, "Test that the SortingAlgorithmController class "
+          + "correctly enqueues a BubbleSort algorithm");
+      assertEquals(SortingAlgorithmType.BUBBLE_SORT, algorithm.getAlgorithmType(),
+          "Test that the algorithm returns the correct algorithm type");
+    } catch (Exception e) {
+      AuxiliaryTestMethods.logExceptionThrown(className, testName);
+      fail("Exception " + e + " thrown while testing enqueueing a bubble sort algorithm into an "
+          + "instance of SortingAlgorithmController; " + e.getMessage());
+    }
+  }
+  
+  @Test
+  void testEnqueueSelectionSort() { // Test 03
+    String testName = "testEnqueueSelectionSort";
+    AuxiliaryTestMethods.logStartTest(className, testName);
+    try {
+      // Enqueue algorithm into a controller
+      SortingAlgorithmController testSortController = new SortingAlgorithmController();
+      testSortController.enqueueAlgorithm(SortingAlgorithmType.SELECTION_SORT, new int[] {1, 3, 2});
+
+      // Retrieve algorithm from the controller and validate the correct algorithm is returned
+      Queue<SortingAlgorithm> tscAlgorithmQueue = testSortController.getAlgorithmQueue();
+      SortingAlgorithm algorithm = tscAlgorithmQueue.remove();
+      assertTrue(algorithm instanceof SelectionSort, "Test that the SortingAlgorithmController "
+          + "class correctly enqueues a SelectionSort algorithm");
+      assertEquals(SortingAlgorithmType.SELECTION_SORT, algorithm.getAlgorithmType(),
+          "Test that the algorithm returns the correct algorithm type");
+    } catch (Exception e) {
+      AuxiliaryTestMethods.logExceptionThrown(className, testName);
+      fail("Exception " + e + " thrown while testing enqueueing a selection sort algorithm into an "
+          + "instance of SortingAlgorithmController; " + e.getMessage());
+    }
+  }
+
+  @Test
+  void testEnqueueInsertionSort() { // Test 04
+    String testName = "testEnqueueInsertionSort";
+    AuxiliaryTestMethods.logStartTest(className, testName);
+    try {
+      // Enqueue algorithm into a controller
+      SortingAlgorithmController testSortController = new SortingAlgorithmController();
+      testSortController.enqueueAlgorithm(SortingAlgorithmType.INSERTION_SORT, new int[] {1, 3, 2});
+
+      // Retrieve algorithm from the controller and validate the correct algorithm is returned
+      Queue<SortingAlgorithm> tscAlgorithmQueue = testSortController.getAlgorithmQueue();
+      SortingAlgorithm algorithm = tscAlgorithmQueue.remove();
+      assertTrue(algorithm instanceof InsertionSort, "Test that the SortingAlgorithmController "
+          + "class correctly enqueues a InsertionSort algorithm");
+      assertEquals(SortingAlgorithmType.INSERTION_SORT, algorithm.getAlgorithmType(),
+          "Test that the algorithm returns the correct algorithm type");
+    } catch (Exception e) {
+      AuxiliaryTestMethods.logExceptionThrown(className, testName);
+      fail("Exception " + e + " thrown while testing enqueueing an insertion sort algorithm into "
+          + "an instance of SortingAlgorithmController; " + e.getMessage());
+    }
+  }
+  
+  @Test
+  void testEnqueueMergeSort() { // Test 05
+    String testName = "testEnqueueMergeSort";
+    AuxiliaryTestMethods.logStartTest(className, testName);
+    try {
+      // Enqueue algorithm into a controller
+      SortingAlgorithmController testSortController = new SortingAlgorithmController();
+      testSortController.enqueueAlgorithm(SortingAlgorithmType.MERGE_SORT, new int[] {1, 3, 2});
+
+      // Retrieve algorithm from the controller and validate the correct algorithm is returned
+      Queue<SortingAlgorithm> tscAlgorithmQueue = testSortController.getAlgorithmQueue();
+      SortingAlgorithm algorithm = tscAlgorithmQueue.remove();
+      assertTrue(algorithm instanceof MergeSort, "Test that the SortingAlgorithmController "
+          + "class correctly enqueues a MergeSort algorithm");
+      assertEquals(SortingAlgorithmType.MERGE_SORT, algorithm.getAlgorithmType(),
+          "Test that the algorithm returns the correct algorithm type");
+    } catch (Exception e) {
+      AuxiliaryTestMethods.logExceptionThrown(className, testName);
+      fail("Exception " + e + " thrown while testing enqueueing a merge sort algorithm into "
+          + "an instance of SortingAlgorithmController; " + e.getMessage());
+    }
+  }
 }
